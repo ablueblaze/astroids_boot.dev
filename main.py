782aecdf986c635384fccb5e1b6c528f55ac6774
Blaze Scott <blazeascott@gmail.com>
2024-09-01 11:37:49 -0500

    Updated main with screen constants

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
+
 import pygame
-import constants
+from constants import *
 
 def main():
+    pygame.init()
     print("Starting asteroids!")
-    print(f"Screen width: {constants.SCREEN_WIDTH}")
-    print(f"Screen height: {constants.SCREEN_HEIGHT}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 if __name__ == "__main__":
     main()
