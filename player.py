f0cba6f4b6fe33ccec022985dab1e1e6a30e3012
Blaze Scott <blazeascott@gmail.com>
2024-09-07 08:59:58 -0500

    Non-functioning commit tracing error

--- player.py
+++ player.py
@@ -1,28 +1,28 @@
-import circleshape
-import constants
+from circleshape import CircleShape
+from constants import *
 import pygame
 
-class Player(circleshape.CircleShape):
+
+class Player(CircleShape):
     def __init__(self, x, y):
-        super().__init__(x, y, constants.PLAYER_RADIUS)
+        super().__init__(x, y, PLAYER_RADIUS)
         # self.radius = constants.PLAYER_RADIUS
         self.rotation = 0
 
+    def draw(self, screen):
+        pygame.draw.polygon(screen, 'white', self.triangle(), 2)
 
     # in the player class
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        # possible break here
+        # example line:
+        # right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
         right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
         a = self.position + forward * self.radius
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
         return [a, b, c]
-
-    def draw(self, screen):
-        pygame.draw.polygon(screen, 'white', self.triangle(), 2)
-
-    def rotate(self, dt):
-        self.rotation += constants.PLAYER_TURN_SPEED * dt
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
@@ -36,7 +36,9 @@ class Player(circleshape.CircleShape):
         if keys[pygame.K_s]:
             self.move(-dt)
 
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
-        self.position += forward * constants.PLAYER_SPEED * dt
-
+        self.position += forward * PLAYER_SPEED * dt
