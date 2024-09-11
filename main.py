7465421766b2ee2b82a1734189a7fb8e8db0b523
Blaze Scott <blazeascott@gmail.com>
2024-09-11 18:26:52 -0500

    Implimented asteroids and movement

--- main.py
+++ main.py
@@ -1,34 +1,33 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     game_clock = pygame.time.Clock()
-
-#    print("Starting asteroids!")
-
-    # Groups for cleaner code base
-    updatable = pygame.sprite.Group()
-    drawable = pygame.sprite.Group()
-
-    Player.containers = (updatable, drawable)
-
-    # Instanciating the pc and adding it to the groups
-    pc = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-
     dt = 0
 
-    # Game loop
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
+    pc = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
+
     while True:
         # Watches for the close button to be pressed
         # Closes the app when it is detected
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
         # Key controls
         for obj in updatable:
             obj.update(dt)
